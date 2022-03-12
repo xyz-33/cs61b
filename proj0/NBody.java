2c53832d63d94692dd4fdfb4e92498a98fa865a7
xzy的Mac <Mac@xzyMacBook-Air.local>
2022-03-12 15:04:28 +0800

    proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -68,12 +68,13 @@ public class NBody{
         	StdDraw.pause(waitTimeMilliseconds);  
         	time = time + dt;      	        	
         }
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < planets.length; i++) {
+    		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                  		planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                 		planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+		}        
     }			
-	StdOut.printf("%d\n", planets.length);
-	StdOut.printf("%.2e\n", radius);
-	for (int i = 0; i < planets.length; i++) {
-    	StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                  	planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                 	planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
-	}	
+	
 }
